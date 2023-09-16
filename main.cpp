a8f09f5e8e6d60e53b256c64e9f831c1908e8fc1
Tárnok Ákos <tarnokakos@live.com>
2023-09-16 19:55:29 +0200

    Update main.cpp
    ki javítva

--- main.cpp
+++ main.cpp
@@ -1,24 +1,21 @@
 #include <iostream> 
 
-constexpr int N_ELEMENTS = 100; // const expr
+constexpr int N_ELEMENTS = 100; 
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // alsó vonás hiányzik rossz a hivatkozás
-    std::cout << '1-100 ertekek duplazasa' // "" ; És kell egy sortörés
-    for (int i = 0;) // hiányos a léptetés és a feltétel
+    int *b = new int[N_ELEMENTS]; 
+    std::cout << "1-100 ertekek duplazasa" << std::endl; 
+    for (int i = 0; i <= N_ELEMENTS; i++) 
     {
-        b[i] = i * 2; // b egy pointer 
+        b[i] = (i + 1) * 2;
+        std::cout << "Ertek:" << b[i] << std::endl;
     }
-    for (int i = 0; i; i++) // hiányzik a fetétel
-    {
-        std::cout << "Ertek:" // a sor lezáratlan nincs ; és kell egy std sortörésnek
-    }   // a két for ciklust lehet egybe kéne rakni 
     std::cout << "Atlag szamitasa: " << std::endl; 
-    int atlag; // kezdõ érték hiányos double vagy float kell típusnak
-    for (int i = 0; i < N_ELEMENTS, i++) // vesszõ helyett ; kell a ciklusban
+    double atlag = 0;; 
+    for (int i = 0; i < N_ELEMENTS; i++) 
     {
-        atlag += b[i] // nincs lezárva  a sor
+        atlag += b[i]; 
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
